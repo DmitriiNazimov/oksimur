953770120ee046bab85347266b2641cb63f623de
DmitriiNazimov <kurs-ssu@mail.ru>
2023-07-26 10:25:31 +0300

    chore(package.json): add lint fix and fix blot

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import postcssMixins from 'postcss-mixins';
 import postcssNested from 'postcss-nested';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-    css: {
-        postcss: {
-            plugins: [postcssMixins, postcssNested],
-        },
+  css: {
+    postcss: {
+      plugins: [postcssMixins, postcssNested],
     },
+  },
+  plugins: [eslint()],
 });
