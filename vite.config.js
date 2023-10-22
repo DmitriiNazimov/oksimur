e8140985b82414d48fd7c240ed4b4d0c69ab7d76
Amongus <amongus@wisebits.com>
2023-10-22 18:23:20 +0300

    feat: add own npm package - vite-plugin-html-inject-localize

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import postcssMixins from 'postcss-mixins';
 import postcssNested from 'postcss-nested';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
-import injectHTML from 'vite-plugin-html-inject';
+import injectHTML from 'vite-plugin-html-inject-localize';
 
 export default defineConfig({
     css: {
