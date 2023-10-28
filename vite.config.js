5b885e271f22149b7a7ef5ef5f98d61b1d36b2ad
Amongus <amongus@wisebits.com>
2023-10-28 17:35:32 +0300

    chore: add img minimizer to vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import postcssNested from 'postcss-nested';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import injectHTML from 'vite-plugin-html-inject-localize';
+import viteImagemin from 'vite-plugin-imagemin';
+
+const imgMinConfig = {
+    gifsicle: { optimizationLevel: 3 },
+    optipng: { optimizationLevel: 5 },
+    mozjpeg: { quality: 60, progressive: true },
+    pngquant: { quality: [0.8, 0.9], speed: 4 },
+    svgo: {
+        plugins: [
+            { removeViewBox: false },
+            { removeEmptyAttrs: false },
+        ],
+    },
+};
 
 export default defineConfig({
     css: {
@@ -10,5 +24,5 @@ export default defineConfig({
             plugins: [postcssMixins, postcssNested],
         },
     },
-    plugins: [eslint(), injectHTML()],
+    plugins: [eslint(), injectHTML(), viteImagemin(imgMinConfig)],
 });
